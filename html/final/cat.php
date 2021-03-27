e7998104fc80b940d8f4470465d85c19e675f790
Kaimuk <kaimook12347@gmail.com>
2021-03-27 15:32:27 +0700

    copy folder php P'Poo

---
+++ html/final/cat.php
@@ -0,0 +1,12 @@
+<?php
+$pageTitle = 'Cat';
+$menus = [
+    ['title' => 'Home', 'url' => 'home.php', 'active' => false],
+    ['title' => 'Map', 'url' => 'map.php', 'active' => true],
+    ['title' => 'Missions', 'url' => 'missions.php', 'active' => false],
+    ['title' => 'Characters', 'url' => 'character.php', 'active' => false],
+    ['title' => 'Store', 'url' => 'store.php', 'active' => false],
+];
+
+include_once('header.php');
+?>
---
+++ html/final/footer.php
@@ -0,0 +1,14 @@
+        </div>
+    </main>
+
+    <?php /*
+    <footer class="footer mt-auto py-3">
+        <div class="container">
+            <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+    </footer>
+    */ ?>
+
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
