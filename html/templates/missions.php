7211c8fb388c8ef6ec04bf6f58dcb76bc7144071
DESKTOP-T31LJOD\Admin <woranitthalaohasomponlert@gmail.com>
2021-03-29 01:30:16 +0700

    mission

--- html/templates/missions.php
+++ html/templates/missions.php
@@ -52,9 +52,15 @@ include_once('header.php');
                             <p class="m-0">อำเภอโคกสำโรง</p>
                             <p class="m-0">จังหวัดลพบุรี 15000</p>
                         </div>
-                        <button class="btn btn-success disabled">Success</button>
+                        <button class="btn btn-success">Let's travel</button>
                     </div>
-
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-xl-10 offset-xl-1 mb-3">
+            <div class="card shadow-sm p-3">
+                <div class="d-flex justify-content-between">
+                    <div class="flex-grow-1 align-self-center d-flex flex-row align-items-center">
                     <img src="img/Mascot_Chiang.png" class="rounded" style="width: 300px;height: 308px;">
                         <div class="ml-4">
                             <h3>Chiangmaa</h3>
@@ -81,6 +87,51 @@ include_once('header.php');
                             <p class="m-0">เชียงใหม่ 50200</p>
                         </div>
                         <button class="btn btn-success">Let's travel</button>
+                    </div>
+                    <div class="border-left px-3">
+                        <h4>Doi Inthanon</h4>
+                        <div class="content mb-3">
+                            <p class="m-0">ตำบลบ้านหลวง</p>
+                            <p class="m-0">อำเภอจอมทอง</p>
+                            <p class="m-0">เชียงใหม่ 50270</p>
+                        </div>
+                        <button class="btn btn-success">Let's travel</button>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-xl-10 offset-xl-1 mb-3">
+            <div class="card shadow-sm p-3">
+                <div class="d-flex justify-content-between">
+                    <div class="flex-grow-1 align-self-center d-flex flex-row align-items-center">
+                    <img src="img/Mascot_Titan.png" class="rounded" style="width: 300px;height: 308px;">
+                        <div class="ml-4">
+                            <h3>Chiangmaa</h3>
+                            <div>
+                                <img src="images/star.png" alt="star"> <img src="images/star.png" alt="star"> <img src="images/star.png" alt="star"> <img src="images/star.png" alt="star">
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="border-left px-3">
+                        <h4>Wat Phra That Doi Suthep</h4>
+                        <div class="content mb-3">
+                            <p class="m-0">ตำบลสุเทพ</p>
+                            <p class="m-0">อำเภอเมือง</p>
+                            <p class="m-0">เชียงใหม่ 50200</p>
+                        </div>
+                        <button class="btn btn-success disabled">Success</button>
+                    </div>
+                    <div class="border-left px-3">
+                        <h4>Tha Non Khon Dean Tha Pea</h4>
+                        <div class="content mb-3">
+                            <p class="m-0">ตำบลศรีภูมิ</p>
+                            <p class="m-0">อำเภอเมือง</p>
+                            <p class="m-0">เชียงใหม่ 50200</p>
+                        </div>
+                        <button class="btn btn-success disabled">Success</button>
                     </div>
                     <div class="border-left px-3">
                         <h4>Doi Inthanon</h4>
