69a5d336e71c7767bb90c98d8a72323620c62371
DESKTOP-T31LJOD\Admin <woranitthalaohasomponlert@gmail.com>
2021-03-29 01:32:10 +0700

    head wrong

--- html/templates/review.php
+++ html/templates/review.php
@@ -1,8 +1,8 @@
 <?php
 $pageTitle = 'Review';
 $menus = [
-    ['title' => 'Map', 'url' => 'map.php', 'active' => true],
-    ['title' => 'Missions', 'url' => 'missions.php', 'active' => false],
+    ['title' => 'Map', 'url' => 'map.php', 'active' => false],
+    ['title' => 'Missions', 'url' => 'missions.php', 'active' => true],
     ['title' => 'Characters', 'url' => 'character.php', 'active' => false],
     ['title' => 'Store', 'url' => 'store.php', 'active' => false],
     ['title' => 'Fighting', 'url' => 'game.php', 'active' => false],
