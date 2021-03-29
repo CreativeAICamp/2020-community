7d5e1b5ee9053b5dd58054a9a8457e53829b40db
DESKTOP-T31LJOD\Admin <woranitthalaohasomponlert@gmail.com>
2021-03-30 00:39:22 +0700

    pie and change name dist to pizzachart

--- html/templates/pie.php
+++ html/templates/pie.php
@@ -8,6 +8,13 @@ $menus = [
     ['title' => 'Fighting', 'url' => 'game.php', 'active' => false],
     ['title' => 'Pie', 'url' => 'pie.php', 'active' => true],
 ];?>
+<?php 
+    $cluster01= 20;
+    $cluster02= 20;
+    $cluster03= 20;
+    $cluster04= 20;
+    $cluster05= 20;
+  ?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html class="no-js lt-ie9" lang="en" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
@@ -23,11 +30,11 @@ $menus = [
     <link rel="stylesheet" href="css/main.css">
 
     
-  <link href="dist/css/vendor/normalize.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="pizzachart/css/vendor/normalize.css" media="screen" rel="stylesheet" type="text/css" />
   <link href="css/main.css">
-  <link href="dist/css/vendor/foundation.css" media="screen" rel="stylesheet" type="text/css" />
-  <link href="dist/css/pizza.css" media="screen, projector, print" rel="stylesheet" type="text/css" />
-  <script src="dist/js/vendor/modernizr.js"></script>
+  <link href="pizzachart/css/vendor/foundation.css" media="screen" rel="stylesheet" type="text/css" />
+  <link href="pizzachart/css/pizza.css" media="screen, projector, print" rel="stylesheet" type="text/css" />
+  <script src="pizzachart/js/vendor/modernizr.js"></script>
   <title><?= $pageTitle ?></title>
 </head>
 <body class="d-flex flex-column h-100" cz-shortcut-listen="true">
@@ -62,77 +69,23 @@ $menus = [
         <div class="content flex-grow-1">
 <body>
   <div class="row">
-    <div class="large-12 columns">
-      <h3>Graphs</h3>
-    </div>
-  </div>
-  <div class="row">
     <div class="large-4 small-4 columns">
       <ul data-pie-id="pie">
-        <li data-value="60">Water Buffalo (60)</li>
-        <li data-value="20">Bison (20)</li>
-        <li data-value="12">Sheep (12)</li>
-        <li data-value="32" data-text="Goats {{percent}} ({{value}} total)">Goat (32)</li>
-        <li data-value="50">Shetland Pony (50)</li>
+        <li data-value="<?php echo $cluster01; ?>">cluster01</li>
+        <li data-value="<?php echo $cluster02; ?>">cluster02</li>
+        <li data-value="<?php echo $cluster03; ?>">cluster03</li>
+        <li data-value="<?php echo $cluster04; ?>">cluster04</li>
+        <li data-value="<?php echo $cluster05; ?>">cluster05</li>
       </ul>
     </div>
     <div class="large-8 small-8 columns">
       <div id="pie"></div>
     </div>
   </div>
-
-  <div class="row">
-    <div class="large-4 small-4 columns">
-      <ul data-pie-id="donut" data-options='{"donut":"true"}'>
-        <li data-value="60">Water Buffalo (60)</li>
-        <li data-value="20">Bison (20)</li>
-        <li data-value="12">Sheep (12)</li>
-        <li data-value="32">Goat (32)</li>
-        <li data-value="50">Shetland Pony (50)</li>
-      </ul>
-    </div>
-    <div class="large-8 small-8 columns">
-      <div id="donut"></div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="large-4 small-4 columns">
-      <ul data-bar-id="bar">
-        <li data-value="200">Water Buffalo (300)</li>
-        <li data-value="178">Bison (178)</li>
-        <li data-value="12">Sheep (12)</li>
-        <li data-value="32">Goat (32)</li>
-        <li data-value="250">Shetland Pony (250)</li>
-        <li data-value="99">Wild Ant (99)</li>
-        <li data-value="78">Chameleon (78)</li>
-      </ul>
-    </div>
-    <div class="large-8 small-8 columns">
-      <div id="bar" style="height: 450px;"></div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="large-4 small-4 columns">
-      <ul data-line-id="line">
-        <li data-y="0" data-x="0">Water Buffalo</li>
-        <li data-y="10" data-x="10">Bison</li>
-        <li data-y="20" data-x="20">Bison</li>
-        <li data-y="30" data-x="30">Bison</li>
-        <li data-y="35" data-x="40">Bison</li>
-        <li data-y="50" data-x="200">Bison</li>
-      </ul>
-    </div>
-    <div class="large-8 small-8 columns">
-      <div id="line" style="height: 450px;"></div>
-    </div>
-  </div>
-
   <br><br><br>
 
-  <script src="dist/js/vendor/dependencies.js"></script>
-  <script src="dist/js/pizza.js"></script>
+  <script src="pizzachart/js/vendor/dependencies.js"></script>
+  <script src="pizzachart/js/pizza.js"></script>
   <script>
     $(window).load(function() {
       Pizza.init();
