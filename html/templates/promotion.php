661189fe8b10544803c277617a4164c171453f32
Kaimuk <kaimook12347@gmail.com>
2021-03-30 15:32:51 +0700

    update promotion

--- html/templates/promotion.php
+++ html/templates/promotion.php
@@ -11,7 +11,6 @@ $menus = [
 
 include_once('header.php');
 ?>
-
 <div class="promo">
     <div class="greenbar">
         <table id="menu_table" >
@@ -25,58 +24,61 @@ include_once('header.php');
                 <div class="coupon">
                     <h>1/5 coupons</h>
                 </div>
-                <div class="o">
-                    <img src="img/Oishi.png" alt="">
-                </div>
-                <div class="text">
-                    <a1 class="name">Oishi</a1>
-                    <a2 class="discount">Discount 5 Bath/bottle</a2>
-                    <a3 class="more">Any favour</a3>
-                </div>
-                <div class="click">
-                    <button class="btn btn-success">1 coupon</button>
-                </div>
-                <div class="banana">
-                    <img src="img/Banana.png" alt="">
-                </div>
-                <div class="text2">
-                    <b1 class="name2">Banana</b1>
-                    <b2 class="discount2">Discount 10 Bath/piece</b2>
-                </div>
-                <div class="click2">
-                    <button class="btn btn-success">1 coupon</button>
-                </div>
-                <div class="milk">
-                    <img src="img/Milk_tea.png" alt="">
-                </div>
-                <div class="text3">
-                    <c1 class="name3">Pasteurized Milk Tea</c1>
-                    <c2 class="discount3">Discount 5 Bath/bottle</c2>
-                    <c3 class="more3">Any favour</c3>
-                </div>
-                <div class="click3">
-                    <button class="btn btn-success">1 coupon</button>
-                </div>
-                <div class="san">
-                    <img src="img/Sandwich.png" alt="">
-                </div>
-                <div class="text4">
-                    <d1 class="name4">Sandwich Nutrition</d1>
-                    <d2 class="discount4">Discount 5 Bath/piece</d2>
-                </div>
-                <div class="click4">
-                    <button class="btn btn-success">1 coupon</button>
-                </div>
-                <div class="dum">
-                    <img src="img/Dump_pings.png" alt="">
-                </div>
-                <div class="text5">
-                    <e1 class="name5">Dump Plings</e1>
-                    <e2 class="discount5">Discount 5 Bath/2 pieces</e2>
-                </div>
-                <div class="click5">
-                    <button class="btn btn-success">1 coupon</button>
-                </div>
+
+                
+                    <div class="o">
+                        <img src="img/Oishi.png" alt="">
+                    </div>
+                    <div class="text">
+                        <a1 class="name">Oishi</a1>
+                        <a2 class="discount">Discount 5 Bath/bottle</a2>
+                        <a3 class="more">Any favour</a3>
+                    </div>
+                    <div class="click">
+                        <button class="btn btn-success">1 coupon</button>
+                    </div>
+                    <div class="banana">
+                        <img src="img/Banana.png" alt="">
+                    </div>
+                    <div class="text2">
+                        <b1 class="name2">Banana</b1>
+                        <b2 class="discount2">Discount 10 Bath/piece</b2>
+                    </div>
+                    <div class="click2">
+                        <button class="btn btn-success">1 coupon</button>
+                    </div>
+                    <div class="milk">
+                        <img src="img/Milk_tea.png" alt="">
+                    </div>
+                    <div class="text3">
+                        <c1 class="name3">Pasteurized Milk Tea</c1>
+                        <c2 class="discount3">Discount 5 Bath/bottle</c2>
+                        <c3 class="more3">Any favour</c3>
+                    </div>
+                    <div class="click3">
+                        <button class="btn btn-success">1 coupon</button>
+                    </div>
+                    <div class="san">
+                        <img src="img/Sandwich.png" alt="">
+                    </div>
+                    <div class="text4">
+                        <d1 class="name4">Sandwich Nutrition</d1>
+                        <d2 class="discount4">Discount 5 Bath/piece</d2>
+                    </div>
+                    <div class="click4">
+                        <button class="btn btn-success">1 coupon</button>
+                    </div>
+                    <div class="dum">
+                        <img src="img/Dump_pings.png" alt="">
+                    </div>
+                    <div class="text5">
+                        <e1 class="name5">Dump Plings</e1>
+                        <e2 class="discount5">Discount 5 Bath/2 pieces</e2>
+                    </div>
+                    <div class="click5">
+                        <button class="btn btn-success">1 coupon</button>
+                    </div>
+                 
             </div>
         </table>
     </div>
